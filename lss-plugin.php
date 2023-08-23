5683d11b4c39c0044d621f96b37ee303b2dfbcb8
cesarioni <cesarioni@hotmail.com>
2023-08-23 16:33:53 +0200

    custom post type fix

--- lss-plugin.php
+++ lss-plugin.php
@@ -14,3 +14,31 @@ Author URI: http://cesarmacias.com
 License: GPL v2 or later
 Text Domain: lss-plugin
 */
+define('ABSPATH') or die('Hey, you can\t access this file, you silly human');
+
+class LssPlugin
+{
+    function __construct(){
+        add_action('init', array($this, 'custom_post_type'));
+    }
+    function activate(){
+
+    }
+    function deactivate(){
+        
+    }
+    function uninstall(){
+        
+    }
+    function custom_post_type(){
+        register_post_type('course', ['online'=>true, 'label'=>'Couses']);
+    }
+}
+if (class_exists('lssPlugin'))
+{
+    $lssPlugin = new LssPlugin();
+}
+
+register_activation_hook(__FILE__,  array($lssPlugin, 'activate'));
+
+register_deactivation_hook(__FILE__,  array($lssPlugin, 'deactivate'));
