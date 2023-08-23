e87fad93e5635bb8ce793853dc4c6617b92ea4eb
cesarioni <cesarioni@hotmail.com>
2023-08-23 15:52:42 +0200

    basic plugin

---
+++ lss-plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *@package lssPlugin 
+ * 
+ */
+/*
+Plugin Name:lss Plugin
+Plugin URI: http://lss.com
+Description: plugin to setup a multipurpose LMS
+Version: 1.0.0
+Author: Cesar Macias
+Author URI: http://cesarmacias.com
+License: GPL v2 or later
+Text Domain: lss-plugin
+*/
